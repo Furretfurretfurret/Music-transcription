760f9f2bde7a14f17115d30ebdd19e0c02bd55ae
Akash Jothi <56031688+akashyftw@users.noreply.github.com>
2023-03-05 20:53:46 -0600

    Added code to try to get song requirements

--- backend/audio_to_midi.py
+++ backend/audio_to_midi.py
@@ -2,4 +2,6 @@
 # import librosa 
 # audio_data, sample_rate = librosa.load('audio_file.wav')
 
+# from librosa.core import piptrack
+# pitch, magnitude = piptrack(audio_data, sr=sample_rate, fmin=50, fmax=2000)
 
